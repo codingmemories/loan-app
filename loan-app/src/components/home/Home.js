39a4b906dd2e0fc77a1d7497cf6c917a511a36c5
codingmemories <vasilisapj89@gmail.com>
2022-06-22 16:26:38 +0300

    add components

---
+++ loan-app/src/components/home/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./Home.css";
+
+export const Home = () => {
+  return (
+    <div className="container">
+      <header className="header">
+        <div className="header_logo">
+          <h1 className="header_logo-title">Current Loans</h1>
+        </div>
+      </header>
+    </div>
+  );
+};
---
+++ loan-app/src/components/index.js
@@ -0,0 +1,2 @@
+export { Home } from "./home/Home";
+export { Modal } from "./modal/Modal.js";
